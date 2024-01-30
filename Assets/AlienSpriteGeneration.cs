84200dee4aadb87ca6430480b9399a8437239083
arsvarg <varganovarsen@gmail.com>
2024-01-30 22:57:04 +0700

    bodypart sprites
    Added packages, created bodypart sprites and prefabs

---
+++ Assets/AlienSpriteGeneration.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSpriteGeneration : MonoBehaviour
+{
+    [Serializefield] private string bodyPrefabPath;
+    GameObject[] bodyPrefabs;
+    [Serializefield] private string headPrefabPath;
+    GameObject[] headPrefabs;
+    [SerializeField] private string facePrefabPath;
+    GameObject[] facePrefabs;
+
+    private void Start()
+    {
+            
+    }
+   public void GenerateSprite()
+    {
+        //generating body
+        
+        
+    }
+   
+    
+   
+  
+}
