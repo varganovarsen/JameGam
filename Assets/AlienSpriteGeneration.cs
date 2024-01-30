d6257614ccd8daf68f0deb434412b9b79724b9e1
arsvarg <varganovarsen@gmail.com>
2024-01-31 03:34:42 +0700

    Loading prefabs int scene

--- Assets/AlienSpriteGeneration.cs
+++ Assets/AlienSpriteGeneration.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class AlienSpriteGeneration : MonoBehaviour
 {
-    [Serializefield] private string bodyPrefabPath;
-    GameObject[] bodyPrefabs;
-    [Serializefield] private string headPrefabPath;
-    GameObject[] headPrefabs;
+    [SerializeField] private string bodyPrefabPath;
+    [SerializeField] private int bodyCount;
+    List<GameObject> bodyPrefabs;
+    [SerializeField] private string headPrefabPath;
+    [SerializeField] private int headCount;
+    List<GameObject> headPrefabs;
     [SerializeField] private string facePrefabPath;
-    GameObject[] facePrefabs;
+    [SerializeField] private int faceCount;
+    List<GameObject> facePrefabs;
 
     private void Start()
     {
-            
+        BodypartsPrefabsLoad();
     }
-   public void GenerateSprite()
+
+    private void BodypartsPrefabsLoad()
+    {
+        bodyPrefabs = new List<GameObject>();
+        for (int i = 1; i == bodyCount; i++)
+        {
+            string path = bodyPrefabPath + i.ToString();
+            GameObject bodyPrefab = Resources.Load<GameObject>(path) as GameObject;
+            bodyPrefabs.Add(bodyPrefab);
+        }
+        
+        headPrefabs = new List<GameObject>();
+        for (int i = 1; i == headCount; i++)
+        {
+            string path = headPrefabPath + i.ToString();
+            GameObject headPrefab = Resources.Load<GameObject>(path) as GameObject;
+            headPrefabs.Add(headPrefab);
+        }
+        
+        facePrefabs = new List<GameObject>();
+        for (int i = 1; i == faceCount; i++)
+        {
+            string path = facePrefabPath + i.ToString();
+            GameObject facePrefab = Resources.Load<GameObject>(path) as GameObject;
+            facePrefabs.Add(facePrefab);
+        }
+    }
+
+    public void GenerateSprite()
     {
         //generating body
         
         
-    }
-   
+    
+     }
+    
     
    
   
