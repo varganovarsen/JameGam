8f6d2b1e6d0b78959eca04296a63d14d70ab853f
arsvarg <varganovarsen@gmail.com>
2024-02-01 19:34:25 +0700

    Meeting aliens, adding to list

---
+++ Assets/AlienDialogue.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienDialogue : MonoBehaviour
+{
+    [SerializeField] public static float startDialogueDistance = 3.5f;
+    
+
+    bool canStartDialogue = false;
+
+    AlienData _data;
+
+    private void OnMouseOver() {
+        if (Vector2.Distance(player_movement.instance.transform.position, transform.position) < startDialogueDistance)
+        {
+            canStartDialogue = true;
+        }
+    }
+
+    
+    private void OnMouseExit()
+    {
+        canStartDialogue = false;
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    private void OnMouseDown()
+    {
+        if (canStartDialogue)
+        {
+            if (!_data.meeted)
+            {
+                Meet();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        _data = GetComponent<AlienData>();
+    }
+
+    public void Meet()
+    {
+        _data.meeted = true;
+
+        //TODO добавление имени в список имён
+        GuestGuessController.instance.MeetGuest(_data);
+        //TODO Обновить таймер исчезнования имени
+        _data.ResetVisibilityTime();
+    }
+}
--- Assets/Scripts/AlienData.cs
+++ Assets/Scripts/AlienData.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class AlienData : MonoBehaviour
 {
 
     public string AlienName;
-
+    public bool meeted = false;
     public Transform AlienMouthPoint;
+
+    [SerializeField] float forgetNameTime = 10f;
+    float _elapsedTime = 0;
+
+    [SerializeField] public CanvasGroup guiLayoutGroup;
+
+    [SerializeField] TMP_Text nameText;
+
+
+    private void Start() {
+        nameText.text = AlienName;
+        guiLayoutGroup.alpha = 0;
+        _elapsedTime = forgetNameTime;
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        Mathf.Clamp(_elapsedTime, 0, forgetNameTime);
+        guiLayoutGroup.alpha = Mathf.Lerp(1 , 0, _elapsedTime / forgetNameTime);
+        
+    }
+    
+    public void ResetVisibilityTime()
+    {
+        _elapsedTime = 0;
+    }
+    
 }
